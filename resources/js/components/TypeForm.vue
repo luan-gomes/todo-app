2e660c3efcf3c9be1e5cd6f4fc6703482463ff8e
Luan Gomes <luangms@outlook.com>
2021-03-06 19:59:09 -0300

    first commit

---
+++ resources/js/components/TypeForm.vue
@@ -0,0 +1,32 @@
+<template>
+  <form>
+    <div class="form-group">
+      <label for="name">Nome do tipo de tarefa</label>
+      <input v-model="form.name" type="text" class="form-control" id="name" />
+      <small id="emailHelp" class="form-text text-muted"
+        >Nunca vamos compartilhar seu email, com ninguém.</small
+      >
+    </div>
+    <div class="row">
+      <div class="col">
+        <button type="submit" class="btn btn-success">Enviar</button>
+      </div>
+      <div class="col">
+        <button class="btn btn-light">Cancelar</button>
+      </div>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: new Form({
+        name: "",
+      }),
+    };
+  },
+  mounted() {},
+};
+</script>
---
+++ resources/views/type/create.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('content')
+    <type-form></type-form>
+@endsection
